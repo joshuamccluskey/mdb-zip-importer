a5b8050375fadafa7d72f28bdb41db03fa9529d2
Josh <jpiff57@gmail.com>
2024-09-19 11:30:13 -0700

    adjustments to work with GitHib actions and new columns in CSV

--- src/main/java/com/example/zipcodeimporter/ZipCode.java
+++ src/main/java/com/example/zipcodeimporter/ZipCode.java
@@ -8,10 +8,17 @@ import lombok.Data;
 @Document(collection = "zipcodes")
 public class ZipCode {
     @Id
-    private String id;
-    private String code;
+    private String zip;
+    private String countyName;
+    private String state;
+    private String stCountyFp;
+    private String classFp;
 
-    public ZipCode(String code) {
-        this.code = code;
+    public ZipCode(String zip, String countyName, String state, String stCountyFp, String classFp) {
+        this.zip = zip;
+        this.countyName = countyName;
+        this.state = state;
+        this.stCountyFp = stCountyFp;
+        this.classFp = classFp;
     }
 }
